4d6bf2e23b65c1df19dcba15064f5439225bcf24
Kenn Villegas <kenn.@phoenix.64.local>
2018-01-11 19:20:18 -0500

    ADDED AKULA PROJECT
    Add.µ
    Add.CD
    Add.HealthKit
    Add.HealthKitUI

---
+++ Akula/Akula/ApplicationSupport/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Akula
+//
+//  Created by Kenn Villegas on 1/11/18.
+//  Copyright © 2018 Kenn Villegas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Akula/Akula/ViewControllers/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Akula
+//
+//  Created by Kenn Villegas on 1/11/18.
+//  Copyright © 2018 Kenn Villegas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
