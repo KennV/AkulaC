fac28d566ddfcf6f83e3b8b981192029472f8bef
Kenn Villegas <kenn.@phoenix.64.local>
2018-01-14 17:24:18 -0500

    ADDED STUB AKULA-TESTS

---
+++ Akula/AkulaTests/AkulaTests/KVAKulaTests.m
@@ -0,0 +1,32 @@
+/**
+  KVAKulaTests.m
+  AkulaTests
+
+  Created by Kenn Villegas on 1/14/18.
+  Copyright © 2018 Kenn Villegas. All rights reserved.
+
+*/
+
+#import <XCTest/XCTest.h>
+#import "KDVAbstractDataController.h"
+#import "KDVApplicationDataController.h"
+
+@interface KVAKulaTests : XCTestCase
+
+@end
+
+@implementation KVAKulaTests
+
+- (void)setUp {
+  [super setUp];
+}
+
+- (void)tearDown {
+  [super tearDown];
+}
+/**
+
+*/
+
+
+@end
