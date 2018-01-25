4c9758dab806f2f8f6a5aa9df6d1452bcfb1e0d8
Kenn Villegas <kenn.@phoenix.64.local>
2018-01-24 19:08:46 -0500

    BRINGING _akulaEntities LIVE
    mostly for the really tight reason that I do not want the view to have
    data I want it to ask for data

--- Akula/Akula/ViewControllers/PrimeTVC/KVPrimeTableViewController.m
+++ Akula/Akula/ViewControllers/PrimeTVC/KVPrimeTableViewController.m
@@ -45,7 +45,7 @@ WOW I thought I did a commit, I was about to add an appDataCon and probably a ge
 @interface KVPrimeTableViewController ()
 
 // HEY THIS switched from mutable akulaEntities to the product of an extensible array
-//@property NSArray *akulaEntities;
+@property (weak, nonatomic)NSArray *akulaEntities;
 
 // so for expediancy I made a cheap CLUT without the table or dict even
 @property (weak, nonatomic)UIColor *baseColor00;
@@ -75,6 +75,8 @@ WOW I thought I did a commit, I was about to add an appDataCon and probably a ge
 @implementation KVPrimeTableViewController
 
 @synthesize ADC = _ADC;
+@synthesize akulaEntities = _akulaEntities;
+
 // Them colors
 @synthesize baseColor00 = _baseColor00;
 @synthesize baseColor01 = _baseColor01;
@@ -96,12 +98,13 @@ WOW I thought I did a commit, I was about to add an appDataCon and probably a ge
 @synthesize hilightTextColor = _hilightTextColor;
 @synthesize specialTextColor = _specialTextColor;
 
--(KVAkulaDataController *)ADC {
-  if (_ADC==(nil)) {
-    _ADC = [[KVAkulaDataController  alloc]initAllUp];
-    NSLog(@"this should have been loaded from the delegate");
-  }
-  return _ADC;
+
+#pragma mark - DataSource
+/**
+ */
+
+- (NSArray *)akulaEntities {
+  return([[self ADC] getAllEntities]);
 }
 
 - (void)viewDidLoad {
@@ -125,9 +128,6 @@ WOW I thought I did a commit, I was about to add an appDataCon and probably a ge
 }
 // TODO: - Replace This with a controller function preferably from a delegate
 - (void)insertNewObject:(id)sender {
-//  if (!self.akulaEntities) {
-//      self.akulaEntities = [[NSMutableArray alloc] init];
-//  }
   // THIS ARRAY IS NO LONGER MUTABLE
 //  [self.akulaEntities insertObject:[NSDate date] atIndex:0];
   NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
@@ -147,28 +147,19 @@ WOW I thought I did a commit, I was about to add an appDataCon and probably a ge
   }
 }
 
-#pragma mark - DataSource
-/**
-// First this must be synthed
-- (NSMutableArray *)entities {
-  return nil;
-}
-
-Hmm, what if akulaEntities - - getAllEntities was a +class function? It doesn't have to be at all but this is where _filthy Singeltons_ come from.
-*/
 #pragma mark - Table View
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
   return 1; // Should be three
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-  return [[_ADC getAllEntities]count];
+  return [[self akulaEntities]count];
 }
   // TODO: - Make a correct Custom Cell
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
   UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
-  // TODO: - fix this;
-  KVRootEntity *object = [_ADC getAllEntities][indexPath.row];
+  
+  KVRootEntity *object = [self akulaEntities][indexPath.row];
   cell.textLabel.text = [[object incepDate]description];
   return cell;
 }
@@ -180,8 +171,9 @@ Hmm, what if akulaEntities - - getAllEntities was a +class function? It doesn't 
 
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
   if (editingStyle == UITableViewCellEditingStyleDelete) {
-    // TODO: - call delete entitiy from the ADC
-    // THIS ARRAY IS NO LONGER MUTABLE
+//    KVRootEntity *deletrix = [[self akulaEntities]objectAtIndex:(indexPath.row)];
+//    [ADC deleteEntity:deletrix];
+    [[self ADC]deleteEntity:[[self akulaEntities]objectAtIndex:(indexPath.row)]];
 //      [self.akulaEntities removeObjectAtIndex:indexPath.row];
       [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
   } else if (editingStyle == UITableViewCellEditingStyleInsert) {
