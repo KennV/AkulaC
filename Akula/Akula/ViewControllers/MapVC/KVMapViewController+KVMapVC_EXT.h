9807a9204c48d9184c08d7985e58a67e1dd52176
Kenn Villegas <kenn.@phoenix.64.local>
2018-06-14 19:43:56 -0400

    STARTING THE GUI

---
+++ Akula/Akula/ViewControllers/MapVC/KVMapViewController+KVMapVC_EXT.h
@@ -0,0 +1,13 @@
+/**
+  KVMapViewController+KVMapVC_EXT.h
+  Akula
+
+  Created by Kenn Villegas on 6/14/18.
+  Copyright © 2018 Kenn Villegas. All rights reserved.
+*/
+
+#import "KVMapViewController.h"
+
+@interface KVMapViewController ()
+
+@end
--- Akula/Akula/ViewControllers/MapVC/KVMapViewController.m
+++ Akula/Akula/ViewControllers/MapVC/KVMapViewController.m
@@ -7,7 +7,6 @@
 This Software, Including its source code, binaries and indermediate derived libraies, objects and data are Propery of Kenneth D. Villegas and are not licensed for Free or Open Source usage. Nor Licensed to be extended by any third party or sub licensee contrator nor entity regardless of any contractural claims otherwise.
 This Remains The Intellectual Property of Kenneth D. Villegas as owner with all Inherent rights reserved under law maintained by Kenneth D. Villegas
 
- 
 */
 
 #import "KVMapViewController.h"
@@ -18,6 +17,16 @@ This Remains The Intellectual Property of Kenneth D. Villegas as owner with all 
 
 @implementation KVMapViewController
 
+#pragma mark - Managing the detail item
+
+- (void)setCurrentEntity:(KVRootEntity *)newDetailItem {
+  if (_currentEntity != newDetailItem) {
+    _currentEntity = newDetailItem;
+    // Update the view.
+    [self configureView];
+  }
+}
+
 - (void)configureView {
   // Update the user interface for the detail item.
   if (self.currentEntity) {
@@ -25,30 +34,15 @@ This Remains The Intellectual Property of Kenneth D. Villegas as owner with all 
   }
 }
 
-
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
   [self configureView];
 }
 
-
 - (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
   // Dispose of any resources that can be recreated.
 }
-
-
-#pragma mark - Managing the detail item
-
-- (void)setCurrentEntity:(KVRootEntity *)newDetailItem {
-  if (_currentEntity != newDetailItem) {
-      _currentEntity = newDetailItem;
-      
-      // Update the view.
-      [self configureView];
-  }
-}
-
 
 @end
