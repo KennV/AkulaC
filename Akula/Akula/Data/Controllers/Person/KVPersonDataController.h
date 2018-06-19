c3a55312d7b23d44ad255d427d0c242d1b988408
Kenn Villegas <kenn.@phoenix.64.local>
2018-06-19 13:00:12 -0400

    ADDED PERSON-DATA-CONTROLLER

---
+++ Akula/Akula/Data/Controllers/Person/KVPersonDataController.h
@@ -0,0 +1,14 @@
+/**
+  KVPersonDataController.h
+  Akula
+
+  Created by Kenn Villegas on 6/19/18.
+  Copyright © 2018 Kenn Villegas. All rights reserved.
+*/
+
+#import <Foundation/Foundation.h>
+#import "KVAkulaDataController.h"
+
+@interface KVPersonDataController <T:KVPerson*> : KVAkulaDataController
+
+@end
---
+++ Akula/Akula/Data/Controllers/Person/KVPersonDataController.m
@@ -0,0 +1,19 @@
+/**
+  KVPersonDataController.m
+  Akula
+
+  Created by Kenn Villegas on 6/19/18.
+  Copyright © 2018 Kenn Villegas. All rights reserved.
+*/
+
+#import "KVPersonDataController.h"
+
+@interface KVPerson ()
+
+- (KVPerson*)PersonAllUp;
+
+@end
+
+@implementation KVPersonDataController
+
+@end
