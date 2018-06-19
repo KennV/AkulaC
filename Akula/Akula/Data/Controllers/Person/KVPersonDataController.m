1d7352a4f4f58b647d4cd1b68beded509e9bde15
Kenn Villegas <kenn.@phoenix.64.local>
2018-06-19 13:53:23 -0400

    SETTING UP BASIC CLASS CONTROLLER

--- Akula/Akula/Data/Controllers/Person/KVPersonDataController.m
+++ Akula/Akula/Data/Controllers/Person/KVPersonDataController.m
@@ -10,10 +10,46 @@
 
 @interface KVPerson ()
 
-- (KVPerson*)PersonAllUp;
+//- (KVPerson*)PersonAllUp;
 
 @end
 
 @implementation KVPersonDataController
+
+- (instancetype)init {
+  return ([self initAllUp]);
+}
+
+
+- (instancetype)initAllUp {
+  return ([self initWithAppName:(@"Akula") databaseName:@"Akula.sqlite" className:@"KVPerson"]);
+}
+
+- (KVPerson*)createPersoninMOC:(NSManagedObjectContext*)m {
+  if (m == nil) {
+    m = [self MOC];
+  }
+  KVPerson * p = [NSEntityDescription insertNewObjectForEntityForName:[self entityClassName] inManagedObjectContext:(m)];
+  KVAbstractPhysics *aep = [NSEntityDescription insertNewObjectForEntityForName:@"KVAbstractPhysics" inManagedObjectContext:(m)];
+  KVAbstractGraphicsEntity * aeg = [NSEntityDescription insertNewObjectForEntityForName:(@"KVAbstractGraphicsEntity") inManagedObjectContext:(m)];
+  KVAbstractLocationEntity * ael = [NSEntityDescription insertNewObjectForEntityForName:(@"KVAbstractLocationEntity") inManagedObjectContext:(m)];
+  
+  [p setIncepDate:[NSDate date]];
+  [p setDbID:[NSUUID UUID]];
+  [p setPhysics:(aep)];
+  [p setGraphics:(aeg)];
+  [p setLocation:(ael)];
+  
+  NSError *error = nil;
+  if (![[self MOC] save:&error]) {
+    NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+    //NSCocoaErrorDomain Code=1570
+    //required fields set to nil see xcdm
+    abort();
+  } else {
+    return p;
+  }
+  
+}
 
 @end
