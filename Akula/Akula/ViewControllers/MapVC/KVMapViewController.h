e63d2ab8f45a4cf595332b0742f3b49481c67d6c
Kenn Villegas <kenn.villegas@Mac.Com>
2018-06-27 17:52:06 -0400

    FIRST PIXEL
    Well that is an abstraction, as the whole thing is graphical -  The actual effect is that I now also have a annotation view whose behavior is undefined. I probably dont even have the delegate set but that is a cool stack of work for today.

--- Akula/Akula/ViewControllers/MapVC/KVMapViewController.h
+++ Akula/Akula/ViewControllers/MapVC/KVMapViewController.h
@@ -15,32 +15,27 @@ This Remains The Intellectual Property of Kenneth D. Villegas as owner with all 
  I STRONGLY SUSPECT that I will be implementing these in the actual controller for person; Or that somehow, and likely quite obvious; I will be a delegate of a delegate in this
  _Theory-WIse_ what it is, is that I have all of the basic db-like functions {actual or swift-proto's} but that is not what the application is. It is the actual behavior of these things. AND I certainly do not want to be doing these here. Rather I should be hitting them off in the ClassController [20180619@1100]
  */
+
 @protocol MapViewActionsProtocol
 
 @optional
-
-
-- (bool)didChangePerson:(id<MapViewActionsProtocol>)deli withPerson:(KVPerson*)p;
-
-- (void)willRunSetupFrom:(id<MapViewActionsProtocol>)deli;
+///Good signatures but these would go into the PersonController
+//- (bool)didChangePerson:(id<MapViewActionsProtocol>)deli withPerson:(KVPerson*)p;
+//- (void)willRunSetupFrom:(id<MapViewActionsProtocol>)deli;
 
 @end
-
 
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
 #import "KVPrimeTableViewController.h"
 
-
 @interface KVMapViewController : UIViewController <MKMapViewDelegate>
-
 //
+@property(weak,nonatomic)id <MapViewActionsProtocol> MA_Delegate;
+
 @property(weak,nonatomic)KVPersonDataController *PDC;
 
-@property(weak,nonatomic)id <MapViewActionsProtocol> MA_Delegate;
 @property(strong,nonatomic)KVRootEntity *currentEntity;
-@property(weak,nonatomic)IBOutlet UILabel *entityDescriptionLabel;
-@property(weak,nonatomic)IBOutlet MKMapView *MapView;
 
 @end
 
--- Akula/Akula/ViewControllers/MapVC/KVMapViewController.m
+++ Akula/Akula/ViewControllers/MapVC/KVMapViewController.m
@@ -13,23 +13,27 @@ This Remains The Intellectual Property of Kenneth D. Villegas as owner with all 
 
 @interface KVMapViewController ()
 
+@property(weak,nonatomic)IBOutlet UILabel *entityDescriptionLabel;
+
+@property(weak,nonatomic)IBOutlet MKMapView *MapView;
+
 @end
 
 @implementation KVMapViewController
-
-- (void)configureView {
-  // Update the user interface for the detail item.
-  if (self.currentEntity) {
-    
-      self.entityDescriptionLabel.text = [self.currentEntity description];
-  }
-}
 
 - (void)viewDidLoad {
   [super viewDidLoad];
   [self setupMapView];
   // Do any additional setup after loading the view, typically from a nib.
   [self configureView];
+}
+
+- (void)configureView {
+  // Update the user interface for the detail item.
+  if (self.currentEntity) {
+    self.entityDescriptionLabel.text = [self.currentEntity description];
+    [self setupNotationPins];
+  }
 }
 
 - (void)didReceiveMemoryWarning {
@@ -55,12 +59,12 @@ This Remains The Intellectual Property of Kenneth D. Villegas as owner with all 
 #pragma mark - Setup GUI State
 
 #pragma mark - Setup Map View
-//TODO: add camera
+
 - (void)setupMapView {
   [[self MapView]setDelegate:self];
 //  MKMapCamera * cam = [[MKMapCamera alloc]init];
   
-  [[self MapView]setMapType:MKMapTypeHybrid];
+  [[self MapView]setMapType:MKMapTypeStandard]; //was MKMapTypeHybrid
   [[self MapView]setShowsScale:false];
 
   if (self.currentEntity == nil ) {
@@ -68,10 +72,12 @@ This Remains The Intellectual Property of Kenneth D. Villegas as owner with all 
       [self setCurrentEntity:(self.PDC.getAllEntities.firstObject)];
     }
   } else {
+    // We do have a proper currentEntity
     CLLocationCoordinate2D objLocation =
     CLLocationCoordinate2DMake([[[_currentEntity location]latitude]doubleValue],
                                [[[_currentEntity location]longitude]doubleValue]);
     MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(objLocation, 500, 500);
+    // ¿bp? ## Observer that at this point the _currEnt is fully fetched with no faults
     [[self MapView]setRegion:region animated:TRUE];
   }
   
@@ -79,6 +85,15 @@ This Remains The Intellectual Property of Kenneth D. Villegas as owner with all 
   
 //  [[self MapView]setCamera:cam];
 }
+
 #pragma mark - Setup Pin View
+- (void)setupNotationPins {
+  KVAkulaDataController *allDataCon = [[KVAkulaDataController alloc]init];
+//  if (![self.PDC.MOC isEqual:(allDataCon.MOC)]) {
+//    NSLog(@"Pony");
+//  }
+  NSMutableArray *j1 = allDataCon.getAllEntities;
+  
+}
 
 @end
