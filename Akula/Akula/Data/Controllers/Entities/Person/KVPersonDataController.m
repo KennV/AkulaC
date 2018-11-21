f7a442be7e9fe8d25e21e0c6227e21191fb68c57
Kenn Villegas <kenn.villegas@Mac.Com>
2018-11-21 15:38:19 -0500

    Update KVPersonDataController.m
    Cleanerize KVPersonDataController

--- Akula/Akula/Data/Controllers/Entities/Person/KVPersonDataController.m
+++ Akula/Akula/Data/Controllers/Entities/Person/KVPersonDataController.m
@@ -17,6 +17,12 @@
 @implementation KVPersonDataController
 @synthesize delegate = _delegate;
 
+/**
+ Couch Front Porch in Baltimore
+ double DefaultLatitude = 37.33115792;
+ double DefaultLongitude = -122.03076853;
+*/
+
 double DefaultLatitude = 37.33115792;
 double DefaultLongitude = -122.03076853;
 
@@ -24,37 +30,25 @@ double DefaultLongitude = -122.03076853;
   return ([self initAllUp]);
 }
 
-
 - (instancetype)initAllUp {
   return ([self initWithAppName:(@"Akula") databaseName:@"Akula.sqlite" className:@"KVPerson"]);
 }
-
-/** -
-- (BOOL)didSetupDefaultLocation:(id<MA_)a forPerson:(id)b fromDelegate:(id)c; {
-  return (false);
-}
- */
 
 - (KVPerson*)makeNewPersonInMOC:(NSManagedObjectContext*)m {
   if (m == nil) {
     m = [self MOC];
   }
   /**
-    Couch Front Porch in Baltimore
-  double DefaultLatitude = 37.33115792;
-  double DefaultLongitude = -122.03076853;
-   */
+   # SIMPLIFICATION *
+   By using the Super's makeNewPersonInMOC:m I can
+   rely on the sub's impl being clean and tidy
+   then I can _set_ the ivars
+  */
   
-  KVPerson * p = [NSEntityDescription insertNewObjectForEntityForName:[self entityClassName] inManagedObjectContext:(m)];
-  KVAbstractPhysics *aep = [NSEntityDescription insertNewObjectForEntityForName:@"KVAbstractPhysics" inManagedObjectContext:(m)];
-  KVAbstractGraphicsEntity * aeg = [NSEntityDescription insertNewObjectForEntityForName:(@"KVAbstractGraphicsEntity") inManagedObjectContext:(m)];
-  KVAbstractLocationEntity * ael = [NSEntityDescription insertNewObjectForEntityForName:(@"KVAbstractLocationEntity") inManagedObjectContext:(m)];
-  
+  KVPerson * p = [super makeNewPersonInMOC:m];
+
   [p setIncepDate:[NSDate date]];
   [p setDbID:[NSUUID UUID]];
-  [p setPhysics:(aep)];
-  [p setGraphics:(aeg)];
-  [p setLocation:(ael)];
   
   if (DefaultLatitude != [[[p location]latitude]doubleValue]) {
     [[p location]setLatitude:[NSNumber numberWithDouble:(DefaultLatitude)]];
@@ -111,13 +105,14 @@ double DefaultLongitude = -122.03076853;
 
 }
 
-//- (void)randomizeContactInformation:(KVPerson*)p {
-//
-//  NSDictionary *jive = @{@"Philadelphia1" : @217, @"Philadelphia2" : @267, @"Bronx1" : @718, @"Bronx2" : @347, @"Bronx3" : @929, @"Connecticut1" : @203, @"Connecticut2" : @860, @"Connecticut3" : @475, @"Portland1" : @503, @"Portland2" : @541, @"Portland3" : @971, @"Portland4" : @458, @"Manhattan1" : @212, @"Manhattan2" : @646, @"Manhattan3" : @332, @"Baltimore1" : @410, @"Baltimore2" : @443, @"Austin1" : @512, @"Austin2" : @737, @"Phoenix1" : @602, @"Phoenix2" : @480, @"Phoenix3" : @520, @"Phoenix4" : @928, @"Phoenix5" : @623, @"Chicago1" : @312, @"Chicago2" : @847, @"Chicago3" : @773, @"Chicago4" : @630, @"Chicago5" : @815};
-////  __unused UInt16 K = [Jive count];
-//  if ([jive count] > 1) {
-//    ;
-//  }
-//}
+/**
+- (void)randomizeContactInformation:(KVPerson*)p {
 
+  NSDictionary *jive = @{@"Philadelphia1" : @217, @"Philadelphia2" : @267, @"Bronx1" : @718, @"Bronx2" : @347, @"Bronx3" : @929, @"Connecticut1" : @203, @"Connecticut2" : @860, @"Connecticut3" : @475, @"Portland1" : @503, @"Portland2" : @541, @"Portland3" : @971, @"Portland4" : @458, @"Manhattan1" : @212, @"Manhattan2" : @646, @"Manhattan3" : @332, @"Baltimore1" : @410, @"Baltimore2" : @443, @"Austin1" : @512, @"Austin2" : @737, @"Phoenix1" : @602, @"Phoenix2" : @480, @"Phoenix3" : @520, @"Phoenix4" : @928, @"Phoenix5" : @623, @"Chicago1" : @312, @"Chicago2" : @847, @"Chicago3" : @773, @"Chicago4" : @630, @"Chicago5" : @815};
+//  __unused UInt16 K = [Jive count];
+  if ([jive count] > 1) {
+    ;
+  }
+}
+*/
 @end
