9614b1d984015e76c56e251f3b44dd3bf9bce710
Kenn Villegas <kenn.villegas@Mac.Com>
2019-01-25 01:44:36 -0500

    ADDED PersonTVC AND ED-VIEW

---
+++ Akula/Akula/ViewControllers/PersonViewControllers/PersonEditor/KVPersonEdViewController.m
@@ -0,0 +1,32 @@
+//
+//  KVPersonEdViewController.m
+//  Akula
+//
+//  Created by Kenn Villegas on 1/25/19.
+//  Copyright © 2019 Kenn Villegas. All rights reserved.
+//
+
+#import "KVPersonEdViewController.h"
+
+@interface KVPersonEdViewController ()
+
+@end
+
+@implementation KVPersonEdViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
