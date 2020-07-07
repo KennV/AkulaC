f8f8c0cc92fa220311e5b53a63dfb487852db370
Kenn Villegas <kenn.villegas@Mac.Com>
2020-07-07 19:56:52 -0400

    FIXING ISSUES IN THE TASKS MODEL - Part 1

--- Akula/Akula/Data/Controllers/Items/Tasks/KVTasksDataController.m
+++ Akula/Akula/Data/Controllers/Items/Tasks/KVTasksDataController.m
@@ -15,10 +15,37 @@
 - (instancetype)initAllUp {
   return ([self initWithAppName:(@"Akula") databaseName:@"Akula.sqlite" className:@"KVTask"]);
 }
-- (KVTask*)makeNewTaskInMOC:(NSManagedObjectContext*)m {
-//  return (KVTask *)([self makeNewEntityInMOC:m]);
-  KVTask *t = ([self makeNewEntityInMOC:m]);
-
+- (KVTask*)makeOLDNewTaskInMOC:(NSManagedObjectContext*)m {
+  KVTask *t =  (KVTask *)([self makeNewEntityInMOC:m]);
+//  KVTask *t = ([self makeNewEntityInMOC:m]);
   return t;
 }
+/**
+OKAY THIS IS WHERE IT IS IN THE SHITS
+ Or at least as far as I can trace. I do seem to be able to make tasks from MapController so that is a benefit. And I have no doubt (*But I need to verify*) that I can also use the MapActions Callbacks to add new People to the PTVC. However
+The Initializer chain for the KVPerson is Far FAR more complete than this one is. Now those are easy to fix but before I waste a few hours on that I would like to add some buttons to the table view controller.
+ 
+ALSO NEED TO FIX: Some autolayout issues in the .nib
+
+*/
+- (KVTask*)makeNewTaskInMOC:(NSManagedObjectContext*)m {
+
+  
+//  if (m == nil) {
+//     m = [self MOC];
+//   }
+   KVTask* entity = (KVTask *)[[KVTask  alloc]initWithContext:m];
+  
+   [entity setIncepDate:[NSDate date]];
+   [entity setDbID:[NSUUID  UUID]];
+   
+//   [entity setPhysics:([super makePhysSubEntityFor:entity In:ctx])];
+//   [entity setLocation:([self makeLocationSubEntityFor:entity In:ctx])];
+//   [entity setGraphics:([self makeGraphicsSubEntityFor:entity In:ctx])];
+   
+   return entity;
+  
+//  return nil;
+}
+
 @end
